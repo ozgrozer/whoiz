8ebfda2128a267ea066a93b6450d5fa67f4400fe
Ozgur Ozer <ozgr@live.com>
2019-09-03 23:51:13 +0300

    Change whois package.

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 1146
-const whois = require('whois-api')
+const whois = require('whois-json')
+
+const whoisQuery = async (props) => {
+  const results = await whois(props.domain)
+  return results
+}
 
 app.use(express.json())
 app.use((req, res, next) => {
@@ -17,14 +22,21 @@ app.listen(port, () => {
 
 app.get('/', (req, res) => res.send('hey'))
 
-app.post('/check', (req, res) => {
+app.post('/check', async (req, res) => {
   const result = {
     success: false
   }
 
-  whois.rawLookup(req.body.domain, (error, data) => {
-    result.whois = data
+  try {
+    const whoisResult = await whoisQuery({
+      domain: req.body.domain
+    })
+
     result.success = true
+    result.whois = whoisResult
     res.send(result)
-  })
+  } catch (e) {
+    result.error = e
+    res.send(result)
+  }
 })
